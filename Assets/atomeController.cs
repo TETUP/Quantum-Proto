bfb542d9ffadc9388d2315fd3a7f39ee97a57f84
TETUP <Mr.tetu@hotmail.fr>
2016-05-04 15:56:16 +0200

    First Mecanique

---
+++ Assets/atomeController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class atomeController : MonoBehaviour {
+
+	public int _coucheMax = 1;
+	public bool touchable = false;
+
+	GameObject _orbit = null;
+	GameObject _electron = null;
+	electronControler _elctrl = null;
+	Light _lgt = null;
+	Renderer _rnder = null;
+	// Use this for initialization
+
+	void Awake () {
+		//Init Gameobjects
+		int i = 0;
+		_orbit = transform.GetChild (0).gameObject;
+		while (i < transform.childCount) {
+			if (transform.GetChild (i).gameObject.name == "Orbit")
+				_orbit = transform.GetChild (i).gameObject;
+			else if (transform.GetChild (i).gameObject.name == "electron") {
+				_electron = transform.GetChild (i).gameObject;
+				_elctrl = _electron.GetComponent<electronControler> ();
+			}
+			i++;
+		}
+
+	}
+	void Start () {
+		//Active orbits
+		int i = 0;
+		for (i = 0; i < _coucheMax; i++)
+			_orbit.transform.GetChild (i).gameObject.SetActive (true);
+
+		_lgt = GetComponent<Light> ();
+		_rnder = GetComponent<Renderer> ();
+
+		if (touchable)
+			Activate ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider c){
+		if (c.gameObject.name == "electron" && _electron != null) {
+			if (_elctrl._couche < _coucheMax) {
+				_elctrl.LoadElec (_coucheMax);
+				if (_elctrl._couche == _coucheMax) {
+					touchable = true;
+					Activate ();
+				}
+			}
+		}
+			
+	}
+
+	void Activate(){
+		touchable = true;
+		_lgt.color = Color.yellow;
+		_rnder.material = _rnder.materials [1];
+		_elctrl.ActivateColor();
+	}
+
+	void OnMouseDown (){
+		if (touchable) {
+			_elctrl.Unleash ();
+			_electron.transform.parent = null;
+			_electron = null;
+			_elctrl = null;
+		}
+	}
+}
--- Assets/electronControler.cs
+++ Assets/electronControler.cs
@@ -5,27 +5,48 @@ public class electronControler : MonoBehaviour {
 
 	GameObject _atome = null;
 	Rigidbody _rb = null;
+	public float _couche = 1;
+	public float position = 1.0f;
 	public float rotateSpeed = 1.0f;
 	private bool leashed = true;
+	private Light _lgt = null;
+
+	void Awake(){
+		_lgt = GetComponent<Light> ();
+	}
 
 	// Use this for initialization
 	void Start () {
 		_atome = transform.parent.gameObject;
 		_rb = GetComponent<Rigidbody> ();
+		//init e-
+		transform.localPosition = new Vector3(0.0f, position, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (leashed)
-			transform.RotateAround(_atome.transform.position, new Vector3(0, 0, 1), rotateSpeed * Time.deltaTime);
+			transform.RotateAround(_atome.transform.position, new Vector3(0, 0, 1), -rotateSpeed * Time.deltaTime);
 	}
 
-	void ChangeOrbit(){
-		
+	void ChangeOrbit(float rayon){
+		transform.localPosition = new Vector3 (0.0f, rayon, 0.0f);
+		transform.rotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f));
 	}
 
-	void Unleash (){
+	public void Unleash (){
 		leashed = false;
-		_rb.AddForce (rotateSpeed, 0.0f, 0.0f);
+		_rb.AddRelativeForce (rotateSpeed, 0.0f, 0.0f);
+	}
+
+	public void LoadElec (int coucheMax){
+		position += 0.5f;
+		_couche++;
+		ChangeOrbit (position);
+	}
+
+	public void ActivateColor (){
+		Debug.Log (_lgt.name);
+		_lgt.color = Color.yellow;
 	}
 }
