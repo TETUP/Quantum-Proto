1d34581aa4ae55b06bddfcd9574fe329be7cd2c2
TETUP <Mr.tetu@hotmail.fr>
2016-05-04 09:55:25 +0200

    First Push

---
+++ Assets/electronControler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class electronControler : MonoBehaviour {
+
+	GameObject _atome = null;
+	Rigidbody _rb = null;
+	public float rotateSpeed = 1.0f;
+	private bool leashed = true;
+
+	// Use this for initialization
+	void Start () {
+		_atome = transform.parent.gameObject;
+		_rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (leashed)
+			transform.RotateAround(_atome.transform.position, new Vector3(0, 0, 1), rotateSpeed * Time.deltaTime);
+	}
+
+	void ChangeOrbit(){
+		
+	}
+
+	void Unleash (){
+		leashed = false;
+		_rb.AddForce (rotateSpeed, 0.0f, 0.0f);
+	}
+}
